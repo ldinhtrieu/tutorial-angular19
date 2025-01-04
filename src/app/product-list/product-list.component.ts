4269b32f740a321cd0edc1ae1a617d2aea9b98e1
ldinhtrieu <ldinhtrieu@gmail.com>
2025-01-04 10:51:25 +0700

    Introducing the Angular HTTP client -Chap08

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -3,19 +3,19 @@ import { Product } from '../product';
 import { ProductDetailComponent } from '../product-detail/product-detail.component';
 import { SortPipe } from '../sort.pipe';
 import { ProductsService } from '../products.service';
-import { ProductViewComponent } from '../product-view/product-view.component';
+
 @Component({
   selector: 'app-product-list',
-  imports: [ProductDetailComponent, SortPipe, ProductViewComponent],
+  imports: [ProductDetailComponent, SortPipe],
   templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.css',
+  styleUrl: './product-list.component.css'
 })
 export class ProductListComponent implements OnInit {
-  products: Product[] = [];
+  products: Product[] = [];  
   selectedProduct: Product | undefined;
 
   constructor(private productService: ProductsService) {}
-
+  
   onAdded() {
     alert(`${this.selectedProduct?.title} added to the cart!`);
   }
